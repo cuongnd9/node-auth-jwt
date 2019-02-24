4480e0a495f195657d18b63d6cc1c52f383482bb
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-02-24 20:49:18 +0700

    🍤 create token

---
+++ app/routes/api.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/user')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+	res.json({ message: 'Welcome to awesome API' })
+})
+
+router.get('/users', async (req, res) => {
+	const users = await User.find()
+	res.json(users)
+})
+
+router.post('/authenticate', async (req, res) => {
+	try {
+		const { username, password } = req.body
+		const user = await User.findOne({ username })
+
+		if (!user) {
+			res.json({ 
+				success: false, 
+				message: 'athentication failed! user not found.'
+			})
+			return
+		}
+
+		if (user.password !== password) {
+			res.json({ 
+				success: false, 
+				message: 'athentication failed! wrong password.'
+			})
+			return
+		}
+
+		const payload = { admin: user.isAdmin }
+		const secretKey = process.env.SECRET
+		const token = jwt.sign(
+			payload, 
+			secretKey, 
+			{ expiresIn: '1h' }
+		)
+
+		res.json({
+			success: true,
+			message: 'token is created!',
+			token
+		})
+	} catch(err) {
+		throw err
+	}
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+const User = require('./app/models/user')
+const apiRoute = require('./app/routes/api')
+
+const app = express()
+
+const port = process.env.PORT || 8080
+
+mongoose.connect(
+	process.env.MONGODB_URI, 
+	{useNewUrlParser: true}
+);
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Chao Xin!' })
+})
+
+app.get('/setup', async (req, res) => {
+	var user = new User({
+		username: 'cuongw',
+		password: 'cuongw',
+		isAdmin: true
+	})
+
+	try {
+		await user.save()
+		res.json({ 
+			success: true, 
+			message: 'user was created!' 
+		})
+	} catch(err) {
+		throw err
+	}
+})
+
+app.use('/api', apiRoute)
+
+app.listen(port, () => 
+	console.log(`Server is running on port ${port}.`)
+)
